18f11f314e714d896a98719929c74d6d2dc4ee7f
lonelyLob0 <abab1000@gmail.com>
2024-05-11 16:46:08 -0600

    Fixed an issue in component Navbar.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,6 @@
 import classes from "@/components/Navbar.module.css";
 import Link from "next/link";
-import PageIcon from "./general/PageIcon";
+import PageIcon from "@/components/general/PageIcon";
 
 export default function Navbar() {
   return (
